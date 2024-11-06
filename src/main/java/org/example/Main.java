d9e432646d920c270e645e34c555c438ebef41ba
Ozmacun <149619575+Ozmacun@users.noreply.github.com>
2024-11-07 02:00:49 +0300

    All tasks done and tested.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Point newPoint = new Point(20,30);
+        System.out.println(newPoint);
+        Point point = new Point(5,6);
+        System.out.println(point.getX());
     }
 }
